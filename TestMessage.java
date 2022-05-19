b4bac0dca61b0802267a8e7903e137b1cd30d9d8
Akashyadav212 <105764955+Akashyadav212@users.noreply.github.com>
2022-05-19 04:37:12 +0000

    Add files via upload

---
+++ TestMessage.java
@@ -0,0 +1,13 @@
+package ecommerce.com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class TestMessage {
+	
+	@GetMapping(value = "/showmessge")
+	 String Message() {
+		return "hi how are you";
+	}
+
+}
